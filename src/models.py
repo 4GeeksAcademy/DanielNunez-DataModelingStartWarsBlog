52ad07b200be411125f06ce87ae6f65da9417b4f
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-08-19 22:13:23 +0000

    Termianl issues

--- src/models.py
+++ src/models.py
@@ -25,6 +25,46 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class User_Favorites(Base):
+    __tablename__ = 'People_Favorites'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False)
+    password = Column(String(200), nullable=False)
+    favorite = relationship(User_Favorites)
+
+class People(Base):
+    __tablename__ = 'People'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False)
+    height = Column(String(100))
+    mass = Column(String(100))
+    hair_color = Column(String(100))
+    skin_color = Column(String(100))
+    eye_color = Column(String(100))
+    birth_year = Column(String(100))
+    gender = Column(String(100))
+    homeworld = Column(String(100))
+    favorite = relationship(User_Favorites)
+
+class Planet(Base):
+    __tablename__ = 'Planet'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False)
+    rotation_period = Column(String(100))
+    orbital_period = Column(String(100))
+    diameter = Column(String(100))
+    climate = Column(String(100))
+    gravity = Column(String(100))
+    favorite = relationship(User_Favorites)
+
     def to_dict(self):
         return {}
 
